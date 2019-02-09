98596d8bd836a6acc24502eaddc82f5c237cca21
jay0lu <lzhen009@gmail.com>
2019-02-08 22:00:37 -0500

    Add UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Modal from "./components/Modal"
 import './App.css';
 
 const todoItems = [
@@ -32,9 +33,49 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      modal: false,
       viewCompleted: false,
-      todoList: todoItems
+      todoList: todoItems,
+      activeItem: {
+        title: "",
+        description: "",
+        completed: false
+      }
     };
+  }
+
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  handleSubmit = (item) => {
+    this.toggle();
+    alert("Save" + JSON.stringify(item));
+  }
+
+  handleDelete = (item) => {
+    alert("Delete" + JSON.stringify(item));
+  }
+
+  createItem = () => {
+    const item = {
+      title: "",
+      description: "",
+      completed: false
+    };
+    this.setState({
+      activeItem: item,
+      modal: !this.state.modal
+    });
+  }
+
+  editItem = (item) => {
+    this.setState({
+      activeItem: item,
+      modal: !this.state.modal
+    });
   }
 
   displayCompleted = status => {
@@ -85,8 +126,8 @@ class App extends Component {
           {item.title}
         </span>
         <span>
-          <button className="btn btn-secondary mr-2">{"Edit"}</button>
-          <button className="btn btn-danger">{"Delete"}</button>
+          <button className="btn btn-secondary mr-2" onClick={() => {this.editItem(item)}}>{"Edit"}</button>
+          <button className="btn btn-danger" onClick={() => {this.handleDelete(item)}}>{"Delete"}</button>
         </span>
       </li>
     ));
@@ -100,7 +141,7 @@ class App extends Component {
         <div className="col-md-6 col-sm-10 mx-auto p-0">
           <div className="card p-3">
             <div className="">
-              <button className="btn btn-primary">Add task</button>
+              <button className="btn btn-primary" onClick={this.createItem}>Add task</button>
             </div>
             {this.renderTabList()}
             <ul className="list-group list-group-flush">
@@ -109,6 +150,13 @@ class App extends Component {
           </div>
         </div>
       </div>
+      {this.state.modal ? (
+        <Modal
+        activeItem={this.state.activeItem}
+        toggle={this.toggle}
+        onSave={this.handleSubmit}
+      />
+      ) : null}
      </main>
     );
   }
